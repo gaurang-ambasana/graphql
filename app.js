947db1ee993532faad7100e69d72a7fbcaf711d0
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-27 23:17:12 +0530

    graphql setup

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import schema from "./schema.js";
+import resolvers from "./resolver.js";
+import { graphqlHTTP as expressGraphQL } from "express-graphql";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
 
 app.get("/", (req, res) => res.status(200).send("API is running..."));
 
