59f363e74c307db50c7e2d5d6078b216b56280b5
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-27 22:05:56 +0530

    test

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from "express";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(3000, () => console.log(`Server is up and running!!`));
+app.get("/", (req, res) => res.status(200).send("API is running..."));
+
+app.listen(PORT, () =>
+  console.log(`Server is up and running!! from Port: ${PORT}`)
+);
